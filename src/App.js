b3166f5c64f99c490a5cf45a0bc446ca0e995ce1
HarshithaRaj13 <harshitharajshekar13@gmail.com>
2024-04-22 17:51:35 +0530

    .

--- src/App.js
+++ src/App.js
@@ -36,3 +36,34 @@ class App extends Component {
 }
 
 export default App;
+
+// import React, { useState } from "react";
+// import Todos from "./Todos";
+// import AddTodo from "./AddTodo";
+
+// const App = () => {
+//   const [todos, setTodos] = useState([
+//     { id: 1, content: "buy some milk" },
+//     { id: 2, content: "play mario cart" },
+//   ]);
+
+//   const deleteTodo = (id) => {
+//     const updatedTodos = todos.filter((todo) => todo.id !== id);
+//     setTodos(updatedTodos);
+//   };
+
+//   const addTodo = (todoContent) => {
+//     const newTodo = { id: Math.random(), content: todoContent };
+//     setTodos([...todos, newTodo]);
+//   };
+
+//   return (
+//     <div className="container">
+//       <h1 className="center blue-text">Todo's</h1>
+//       <Todos todos={todos} deleteTodo={deleteTodo} />
+//       <AddTodo addTodo={addTodo} />
+//     </div>
+//   );
+// };
+
+// export default App;
